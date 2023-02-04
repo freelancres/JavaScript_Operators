ade033e84388c0690a0607e53a33a8ba083660a4
Safwan Kher Allah <68548982+safwan-kher@users.noreply.github.com>
2023-02-04 08:25:57 +0100

    Add files via upload

---
+++ exercises/solution.js
@@ -0,0 +1,48 @@
+//Math Problems
+
+//Q1. In one night, a movie theater sells tickets for 6450 dollars. Each ticket costs 15 dollars. How many people purchased a ticket?
+const ticketsTotalAmount = 6450;
+const ticketPrice = 15;
+const numberOfPeople = ticketsTotalAmount / ticketPrice; 
+console.log(`01: ${numberOfPeople} bought tickets`);
+
+//Q2. Sylvia's income is 500 dollars per week. How much does Sylvia makes every year?
+console.log(`02: Beträg in Jahr = ${500 * 52} `);
+//Q3. Calculate the percentage of 17/30. Expected output: number%
+
+let x = 17;
+let y = 30;
+let percentage = x * 100 /y;
+console.log(`Q3: ${percentage}% `);
+
+//Q4. Calculate the perimeter of a square. Assume each side is 4.75cm.
+const sideLenght = 4.75;
+const Perimeter = sideLenght * 4;
+console.log(`04: Perimeter of square is ${Perimeter}cm`);
+
+//Q5. Calculate the perimeter of a triangle. Assume the length of the sides are 5cm, 6cm, 7cm.
+console.log(`05: Perimeter ofa triangle is ${5+6+7}cm`);
+//Q6. Calculate the area of a square. Each side is 5cm.
+console.log(`06: Area of squar is ${5 ** 2}cm²`);
+//Q7. Calculate the area of a right-angled triangle. Assume the length of the sides are 3cm, 4cm, 5cm.
+// a right-angled triangle is half of a rectangle shaped by the "legs" of the trigangle (gr. catheti)
+
+console.log(`07: The area of a right-angled triangle is ${(3 * 4) / 2}cm² `);
+
+console.log(4 / 2 * 3);
+
+//Q8. Calculate the volume of a cube. Length of each side is 9cm.
+
+//Q9. Calculate the three bills including tips:
+//€22.35 + 10% tip
+//€26.67 + 15% tip
+//€35.92 + 20% tip
+
+//Q10. The number of hours Noemy worked over the last two weeks are 8, 6, 5, 9, 8, 2, 1, 8.5, 7, 4
+//What is Noemy's average hour?
+
+//Q11. A math student scored 75, 70, 85, 90, 100 on the first five tests he took . After he took his sixth math test, the average is now 85. What did he score on the sixth test?
+// Expected output: Score in the sixth test: --.
+
+//Q12. For James to get a 1st class degree, he needs to get an average of 80% in all 9 of his assessments. He has taken 8 assessments and his average is 78%. What is the minimum percentage he must get in his last assessment to ensure he gets a first class?
+//Expected output: James needs a minimum of --% to get an 80% average.
---
+++ live-code/script.js
@@ -0,0 +1,103 @@
+/*-------------- Arithmetic Operators-----------------*/
+
+
+let x = 6, y = 3;
+
+//console.log(x + y);
+//console.log(x - y);
+//console.log(x * y);
+//console.log(x / y);
+//console.log(x ** y) // ES2016 : Exponentiation
+
+//console.log(x % y); // Remainder
+
+// Post increment
+// console.log(y++); 3
+y++; // 3
+//console.log(y) // 4
+
+//Pre increment
+//console.log(++x); // 7
+
+// Post decrement
+
+//console.log(y--); //4
+//console.log(y); // 3
+
+// Pre decrement
+//console.log(--x); // 6
+
+/*-------------- Assignment Operators-----------------*/
+let d = 9;
+let g;
+
+g = d;
+
+//console.log(d, g);
+//console.log(g += d); // g = g + d ; g 18 , d 9
+//console.log(d += 10); // d 19
+
+//console.log(g -= 2); // 16
+//console.log(g *= 2); // 32
+//console.log(d %= 2); // 1
+
+/*-------------- Comparison Operators-----------------*/
+let a = 10, b = 25, c =10;
+// equal value
+//console.log(a == 10); // true
+// equal value
+//console.log(a == '10'); // true
+//console.log(Number('10')); // 10
+// equal value or type
+//console.log(a === '10'); // false
+
+// not equal value
+//console.log(a != 10); // false
+// not equal value 
+//console.log(a != '10'); // false
+
+// not equal value or type
+//console.log(a !== 10); // false
+
+// greater than
+
+//console.log(a > b); // false
+
+// less than
+
+//console.log(a < b); // true
+
+// greater than or equal 
+//console.log(a >= c); // true
+
+// less than or equal 
+//console.log(a <= c); // true
+
+
+/*-------------- Logical Operators-----------------*/
+/**
+ * &&
+ * ||
+ *  !
+ * 
+ */
+
+let t = true, f = false;
+//' &&'
+//console.log(t && t);
+//console.log(t && f);
+//console.log(f && f);
+//console.log(f && t);
+
+// '||'
+//console.log('---------')
+//console.log(t || t);
+//console.log(t || f);
+//console.log(f || f);
+//console.log(f || t);
+
+// '!'
+//console.log(!t);
+//console.log(!f);
+
+
